40e3680e1f25b2d949b47721a2b2a5fa5cdb20dc
Benjamín Vicente <benjavicente@uc.cl>
2021-08-23 12:24:34 -0400

    actualizar scripts

--- scripts/reduce_imgs.py
+++ scripts/reduce_imgs.py
@@ -2,22 +2,34 @@ import subprocess
 from pathlib import Path
 import sys
 
-def main(size = "600x800", *, bg_color = "black"):
-    for img_path in Path.cwd().glob("**/*"):
+
+CWD = str(Path.cwd())
+STATIC_IMG_STRATEGY = [
+    "-quality", "80",
+    "-define", "png:compression-level=9",
+    "-define", "png:compression-filter=5",
+    "-define", "png:compression-strategy=1",
+]
+GIF_STRATEGY = ["-coalesce", "-deconstruct"]
+
+def main(path=CWD, size="600x800", bg_color="white"):
+    for img_path in Path(path).glob("**/*"):
         if not img_path.suffix.endswith(("png", "jpg", "gif")):
             continue
+
+        print(f"Compresing {img_path}")
+        is_static = img_path.suffix.endswith(("png", "jpg"))
 
         subprocess.run([
           "convert",
           img_path.resolve(),
-          "-quality", "80",
-          "-define", "png:compression-level=9",
-          "-define", "png:compression-filter=5",
-          "-define", "png:compression-strategy=1",
+          *([GIF_STRATEGY[0]] if not is_static else []),
+          *(STATIC_IMG_STRATEGY if is_static else []),
           "-resize", size,
           "-background", bg_color,
           "-gravity", "center",
           "-extent", size,
+          *([GIF_STRATEGY[1]] if not is_static else []),
           img_path.resolve()
         ])
 
